c460e757b14ef1120bbf5a88f6aff255a6974ecb
AntonellaApex <168466789+AntonellaApex@users.noreply.github.com>
2026-02-06 08:40:23 -0800

    .

--- tests/search.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Pokemon Search', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  // Bonus tests - implement if you have time
-});
